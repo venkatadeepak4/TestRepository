c5c5b2c45ccecfe12f0965526962bb5a0a50c56e
deepu <deepu@deepu-pc>
2018-05-20 19:19:01 +0530

    test

--- deepakbatch/src/main/java/hello/Application.java
+++ deepakbatch/src/main/java/hello/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+	// Main application
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
