6c85a4e3f5c7611a37f6ff9d8773a7cd0576d6d3
deepu <deepu@deepu-pc>
2019-11-13 20:32:30 +0530

    My test

---
+++ MySpringBootApp/src/main/java/hello/ClueHOMultiController.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package hello;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author deepu
+ *
+ */
+@RestController
+public class ClueHOMultiController {
+
+}
---
+++ MySpringBootApp/src/main/webapp/WEB-INF/jsp/home.jsp
@@ -0,0 +1,8 @@
+<html>
+   <head>
+      <title>Hello Spring MVC</title>
+   </head>
+   <body>
+      <h2>test</h2>
+   </body>
+</html>
